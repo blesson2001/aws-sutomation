e74c6731f00ddeed0f91746ad50f24ae3c014faa
blesson2001 <blesson2001@gmail.com>
2021-05-15 02:26:07 +0000

    thisisfine

--- createuser.sh
+++ createuser.sh
@@ -3,3 +3,5 @@ aws iam create-user --user-name kimo --permissions-boundary arn:aws:iam::aws:pol
 aws iam create-access-key --user-name kimo
 
 aws iam create-login-profile --user-name kimo --password 's*tlL-UcP_wG}+6' --no-password-reset-required
+
+aws iam attach-user-policy --user-name kimo --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
